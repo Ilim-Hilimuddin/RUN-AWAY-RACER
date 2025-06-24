7a2aaa5b525b93bb64b433b7337ca0a6a3b54e66
Ilim-Hilimuddin <hilimuddin@gmail.com>
2025-06-24 20:12:03 +0700

    update

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,43 +1,63 @@
 extends Control
-@onready var heart_1 = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer2/Label4/Label/HBoxContainer/Heart1
-@onready var heart_2 = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer2/Label4/Label/HBoxContainer/Heart2
-@onready var heart_3 = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer2/Label4/Label/HBoxContainer/Heart3
-@onready var game_over = $CanvasLayer/GameOver
-@onready var game_ui = $CanvasLayer/GameUi
-@onready var high_score_label = $CanvasLayer/GameOver/HigScoreLabel
-@onready var your_score = $CanvasLayer/GameOver/YourScore
-@onready var score = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer/UI1/Score
-@onready var coin = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer/UI2/coin
-@onready var high_score = $CanvasLayer/GameUi/HBoxContainer/VBoxContainer/UI3/HighScore
 
-var hearts=[]
+var hearts = []
 
 func _ready():
+	# Ambil semua node UI secara fleksibel
+	var heart_1 = find_child("Heart1", true, false)
+	var heart_2 = find_child("Heart2", true, false)
+	var heart_3 = find_child("Heart3", true, false)
 	hearts = [heart_1, heart_2, heart_3]
-	
+
+	var retry = find_child("Retry", true, false)
+	var exit = find_child("Exit", true, false)
+	retry.pressed.connect(_on_retry_pressed)
+	exit.pressed.connect(_on_exit_pressed)
+
 func _process(delta):
 	var game_manager = get_node("/root/Main/GameManager")
-	var live = get_node("/root/Main/Car").current_health
-	coin.text = str(game_manager.coin)
-	score.text = str(game_manager.score)
-	var hs=game_manager.highscore
-	var sc=game_manager.score+game_manager.coin
-	if sc>hs:
-		game_manager.highscore=sc
-		hs=sc
-	high_score.text=str(hs)
+	var car = get_node("/root/Main/Car")
+	var live = car.current_health
+
+	var coin_label = find_child("coin", true, false)
+	var score_label = find_child("Score", true, false)
+	var high_score_label_ui = find_child("HighScore", true, false)
+	var high_score_result = find_child("HigScoreLabel", true, false)
+	var your_score_label = find_child("YourScore", true, false)
+	var game_over = find_child("GameOver", true, false)
+	var game_ui = find_child("GameUi", true, false)
+
+	# Hitung dan tampilkan skor
+	var hs = game_manager.highscore
+	var sc = game_manager.score + game_manager.coin
+
+	if sc > hs:
+		game_manager.highscore = sc
+		hs = sc
+
+	score_label.text = str(game_manager.score)
+	coin_label.text = str(game_manager.coin)
+	high_score_label_ui.text = str(hs)
+
 	update_hearts(live)
-	if live==0:
+
+	if live == 0:
 		get_tree().paused = true
 		game_over.visible = true
-		game_ui.visible=false
+		game_ui.visible = false
 		game_manager.save_highscore(sc)
-		high_score_label.text=str(hs)
-		your_score.text=str(sc)	
-			
+		high_score_result.text = str(hs)
+		your_score_label.text = str(sc)
+
 func update_hearts(live):
 	for i in range(hearts.size()):
 		hearts[i].visible = i < live
-	
 
-	
+func _on_retry_pressed():
+	var game_over = find_child("GameOver", true, false)
+	if game_over.visible:
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
